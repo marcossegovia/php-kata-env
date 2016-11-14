53344710cce361044e187b8dede1145f7750f81c
Marcos Segovia <marcos.segovia@uvinum.com>
2016-11-14 17:19:22 +0100

    Removing php 5.6 and hhvm from travis. Using VcsStream in integrations tests.

--- tests/Integration/Example/Context/ExampleContext.php
+++ tests/Integration/Example/Context/ExampleContext.php
@@ -4,31 +4,48 @@ namespace KataTest\Integration\Example\Context;
 
 use Behat\Behat\Context\Context;
 use Behat\Behat\Hook\Scope\AfterScenarioScope;
+use Behat\Behat\Hook\Scope\BeforeScenarioScope;
 use Behat\Gherkin\Node\PyStringNode;
+use org\bovigo\vfs\vfsStream;
+use org\bovigo\vfs\vfsStreamDirectory;
+use org\bovigo\vfs\vfsStreamFile;
 
 final class ExampleContext implements Context
 {
+    /** @var vfsStreamDirectory */
+    private $root;
+
+    /** @var vfsStreamDirectory */
+    private $directory;
+
+    /** @var vfsStreamFile */
+    private $file;
+
+    private $output;
+
+    /**
+     * @BeforeScenario @file
+     */
+    public function setUp(BeforeScenarioScope $scope)
+    {
+        $this->root = vfsStream::setup('test_base');
+    }
+
     /**
      * @AfterScenario @file
      */
     public function cleanDir(AfterScenarioScope $scope)
     {
-        chdir("../");
-        exec("rm -rf test");
+        $this->root = vfsStream::setup('test_base');
     }
-
-    private $output;
 
     /**
      * @Given I am in a directory :dir
      */
     public function iAmInADirectory($dir)
     {
-        if (!file_exists($dir))
-        {
-            mkdir($dir);
-        }
-        chdir($dir);
+        $this->directory = vfsStream::newDirectory($dir);
+        $this->root->addChild($this->directory);
     }
 
     /**
@@ -36,15 +53,21 @@ final class ExampleContext implements Context
      */
     public function iHaveAFileNamed($file)
     {
-        touch($file);
+        $this->file = vfsStream::newFile($file);
+        $this->file->at($this->directory);
     }
 
     /**
-     * @When I run :arg1
+     * @When I list the current directory
      */
-    public function iRun($command)
+    public function iListTheCurrentDirectory()
     {
-        exec($command, $output);
+        $files = $this->directory->getChildren();
+        $output = [];
+        foreach($files as $file)
+        {
+            $output[] = $file->getName();
+        }
         $this->output = trim(implode("\n", $output));
     }
 
--- tests/Integration/Example/Feature/example.feature
+++ tests/Integration/Example/Feature/example.feature
@@ -1,14 +1,14 @@
 @file
-Feature: ls
+Feature: List directory
   In order to see the directory structure
-  As a UNIX user
-  I need to be able to list the current directory's contents
+  As a user
+  I need to be able to list the current directory's content
 
 Scenario: List 2 files in a directory
   Given I am in a directory "test"
-  And I have a file named "foo"
   And I have a file named "bar"
-  When I run "ls"
+  And I have a file named "foo"
+  When I list the current directory
   Then I should get:
     """
     bar
