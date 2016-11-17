5cde7a47629b2401e7e625eb088e25626ae8776d
Albert Garcia <albert.garcia@uvinum.com>
2016-11-17 10:07:24 +0100

    Calculate score based on partial scoring tries

--- src/BowlingKata/Bowling.php
+++ src/BowlingKata/Bowling.php
@@ -4,8 +4,34 @@ namespace Kata\BowlingKata;
 
 final class Bowling
 {
-    public function __invoke(array $frames) : int
+    /** @var int */
+    private $score;
+
+    public function __invoke(array $tries) : int
     {
-        return $frames[0];
+        foreach ($tries as $try)
+        {
+            $this->throw($try);
+        }
+
+        return $this->score();
+    }
+
+    private function throw($try)
+    {
+        if ($this->isPartialScoring($try))
+        {
+            $this->score += $try;
+        }
+    }
+
+    private function score() : int
+    {
+        return $this->score;
+    }
+
+    private function isPartialScoring($try)
+    {
+        return is_int($try);
     }
 }
--- tests/Unit/BowlingKata/BowlingTest.php
+++ tests/Unit/BowlingKata/BowlingTest.php
@@ -19,9 +19,9 @@ class BowlingTest extends \PHPUnit_Framework_TestCase
 		return array(
 			'testThrowing9PinsInFirstFrame'								=> array( array( 9, '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-' ), 9 ),
 			'testThrowing5PinsInFirstFrame'								=> array( array( 5, '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-' ), 5 ),
-//			'testThrowing12PinsInFirstFrameWithoutStrike'				=> array( array( 9, 3, '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-' ), 12 ),
-//			'testThrowing12PinsInSecondFrameWithoutStrike'				=> array( array( '-', '-', 9, 3, '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-' ), 12 ),
-//			'testThrowing12PinsBetweenFirstAndSecondFrameWithoutStrike'	=> array( array( 9, '-', '-', 3, '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-' ), 12 ),
+			'testThrowing12PinsInFirstFrameWithoutStrike'				=> array( array( 9, 3, '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-' ), 12 ),
+			'testThrowing12PinsInSecondFrameWithoutStrike'				=> array( array( '-', '-', 9, 3, '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-' ), 12 ),
+			'testThrowing12PinsBetweenFirstAndSecondFrameWithoutStrike'	=> array( array( 9, '-', '-', 3, '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-' ), 12 ),
 //			'testThrowingSpareInFirstFrameAnd2PinsInSecondIs14'			=> array( array( 5, '/', 2, '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-' ), 14 ),
 //			'testThrowingSpareInFirstFrameAnd2And3PinsInSecondIs17'		=> array( array( 5, '/', 2, 3, '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-' ), 17 ),
 //			'testOneStrikeInFirstThrowIs10'								=> array( array( 'X', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-' ), 10 ),
