ba9024467eceb7f73cc56d9c7206fc36b0f71d7a
Xavier Serrat Bordas <xavier.serrat22@gmail.com>
2016-11-18 00:41:45 +0100

    Bowlingkata/xavi: Check if a try has a value of 'strike' and calculate the punctuation for this case

--- src/BowlingKata/Bowling.php
+++ src/BowlingKata/Bowling.php
@@ -10,6 +10,12 @@ final class Bowling
 
         foreach ($tries as $num_try => $try)
         {
+            if ($this->isATryWithStrike($try))
+            {
+                $current_punctuation = 10 + $tries[$num_try+1] + $tries[$num_try+2];
+                $punctuation+= $current_punctuation;
+                continue;
+            }
 
             if ($this->isAFrameWithSpare($tries, $num_try))
             {
@@ -28,5 +34,10 @@ final class Bowling
     private function isAFrameWithSpare($tries, $num_try)
     {
         return isset($tries[$num_try+1]) && $tries[$num_try+1]  === '/';
+    }
+
+    private function isATryWithStrike($try)
+    {
+        return $try === 'X';
     }
 }
