adcb24bfc219be47c703cce4ae22f1a02ceeffde
Albert Garcia <albert.garcia@uvinum.com>
2016-12-15 13:31:48 +0100

    Removed unnecessary breaks on `ItemFactory`

--- src/GildedRoseKata/ItemFactory.php
+++ src/GildedRoseKata/ItemFactory.php
@@ -6,20 +6,15 @@ final class ItemFactory
 {
     public static function fromItem(Item $item)
     {
-        switch($item->name)
-        {
+        switch ($item->name) {
             case AgedBrieItem::ITEM_NAME:
                 return new AgedBrieItem($item);
-                break;
             case SulfurasItem::ITEM_NAME:
                 return new SulfurasItem($item);
-                break;
             case BackstagePassItem::ITEM_NAME:
                 return new BackstagePassItem($item);
-                break;
             case ConjuredItem::ITEM_NAME:
                 return new ConjuredItem($item);
-                break;
             default:
                 return new BaseItem($item);
         }
