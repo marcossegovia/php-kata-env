37fc8967ba7d4a4204bda1690d5322f36e5fb976
Eduard Fabra Bori <eduard.fabra@uvinum.com>
2016-12-15 10:31:01 +0100

    Apply some guard clauses

--- src/GildedRoseKata/GildedRose.php
+++ src/GildedRoseKata/GildedRose.php
@@ -2,46 +2,65 @@
 
 namespace Kata\GildedRoseKata;
 
-class GildedRose {
+class GildedRose
+{
 
     private $items;
 
-    function __construct($items) {
+    function __construct($items)
+    {
         $this->items = $items;
     }
 
-    function update_quality() {
-        foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+    function update_quality()
+    {
+        foreach ($this->items as $item)
+        {
+            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert')
+            {
                 $this->decrementQualityOneUnitIfItemNameIsNotSulfuras($item);
-            } else {
-                if ($item->quality < 50) {
+            }
+            else
+            {
+                if ($item->quality < 50)
+                {
                     $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sell_in < 11) {
+                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert')
+                    {
+                        if ($item->sell_in < 11)
+                        {
                             $this->increaseQualityByOneUnitWhenQualitySmallerThanFifty($item);
                         }
-                        if ($item->sell_in < 6) {
+                        if ($item->sell_in < 6)
+                        {
                             $this->increaseQualityByOneUnitWhenQualitySmallerThanFifty($item);
                         }
                     }
                 }
             }
 
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+            if ($item->name != 'Sulfuras, Hand of Ragnaros')
+            {
                 $item->sell_in = $item->sell_in - 1;
             }
 
-            if ($item->sell_in < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        $this->decrementQualityOneUnitIfItemNameIsNotSulfuras($item);
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
-                    $this->increaseQualityByOneUnitWhenQualitySmallerThanFifty($item);
-                }
+            if ($item->sell_in > 0)
+            {
+                continue;
+            }
+
+            if ($item->name == 'Aged Brie')
+            {
+                $this->increaseQualityByOneUnitWhenQualitySmallerThanFifty($item);
+            }
+
+            if ($item->name != 'Backstage passes to a TAFKAL80ETC concert')
+            {
+                $this->decrementQualityOneUnitIfItemNameIsNotSulfuras($item);
+            }
+            else
+            {
+                $item->quality = $item->quality - $item->quality;
             }
         }
     }
