45f9dec324989d87a527d1459fc8114c87c930c8
Marcos <velozmarkdrea@gmail.com>
2017-03-23 10:06:39 +0100

    Applying reformatting

--- src/Algorithm/Finder.php
+++ src/Algorithm/Finder.php
@@ -19,41 +19,50 @@ final class Finder
         /** @var Comparator[] $comparators */
         $comparators = [];
 
-        for ($i = 0; $i < count($this->people); $i++) {
-            for ($j = $i + 1; $j < count($this->people); $j++) {
+        for ($i = 0; $i < count($this->people); $i++)
+        {
+            for ($j = $i + 1; $j < count($this->people); $j++)
+            {
                 $r = new Comparator();
 
-                if ($this->people[$i]->birthDate < $this->people[$j]->birthDate) {
-                    $r->first_person = $this->people[$i];
-                    $r->second_person           = $this->people[$j];
-                } else {
-                    $r->first_person = $this->people[$j];
-                    $r->second_person           = $this->people[$i];
+                if ($this->people[$i]->birthDate < $this->people[$j]->birthDate)
+                {
+                    $r->first_person  = $this->people[$i];
+                    $r->second_person = $this->people[$j];
+                }
+                else
+                {
+                    $r->first_person  = $this->people[$j];
+                    $r->second_person = $this->people[$i];
                 }
 
-                $r->difference_number = $r->second_person->birthDate->getTimestamp()
-                    - $r->first_person->birthDate->getTimestamp();
+                $r->difference_number = $r->second_person->birthDate->getTimestamp() - $r->first_person->birthDate->getTimestamp();
 
                 $comparators[] = $r;
             }
         }
 
-        if (count($comparators) < 1) {
+        if (count($comparators) < 1)
+        {
             return new Comparator();
         }
 
         $first = $comparators[0];
 
-        foreach ($comparators as $result) {
-            switch ($sorting_type) {
+        foreach ($comparators as $result)
+        {
+            switch ($sorting_type)
+            {
                 case Sorting::HIGHER_TO_LOWER:
-                    if ($result->difference_number < $first->difference_number) {
+                    if ($result->difference_number < $first->difference_number)
+                    {
                         $first = $result;
                     }
                     break;
 
                 case Sorting::LOWER_TO_HIGHER:
-                    if ($result->difference_number > $first->difference_number) {
+                    if ($result->difference_number > $first->difference_number)
+                    {
                         $first = $result;
                     }
                     break;
