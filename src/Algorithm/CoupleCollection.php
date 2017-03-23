fe45d4ad8f6ea99b18edfe8b96532057d1892699
Albert Garcia <albert.garcia@uvinum.com>
2017-03-23 10:50:45 +0100

    Replace Couple array by CoupleCollection, so we can encapsulate de couples generation and extract it from the Finder.

---
+++ src/Algorithm/CoupleCollection.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Kata\Algorithm;
+
+class CoupleCollection implements \Iterator, \Countable, \ArrayAccess
+{
+    /** @var Couple[] */
+    private $all_items;
+
+    /**
+     * @param Couple[] $couples_array
+     */
+    private function __construct(array $couples_array)
+    {
+        $this->all_items = $couples_array;
+    }
+
+    /**
+     * @param array $people_array
+     *
+     * @return CoupleCollection
+     */
+    public static function buildAllPossibleCouplesFromPeopleArray(array $people_array): CoupleCollection
+    {
+        $couples = [];
+        for ($i = 0; $i < count($people_array); $i++) {
+            for ($j = $i + 1; $j < count($people_array); $j++) {
+                $couples[] = new Couple($people_array[$i], $people_array[$j]);
+            }
+        }
+
+        return new self($couples);
+
+    }
+
+    public function allItems()
+    {
+        return $this->all_items;
+    }
+
+    public function offsetExists($offset)
+    {
+        return isset($this->all_items[$offset]);
+    }
+
+    public function offsetGet($offset)
+    {
+        return $this->all_items[$offset];
+    }
+
+    public function offsetSet($offset, $value)
+    {
+        return $this->all_items[$offset] = $value;
+    }
+
+    public function offsetUnset($offset)
+    {
+        unset($this->all_items[$offset]);
+    }
+
+    public function count()
+    {
+        return count($this->all_items);
+    }
+
+
+    public function current()
+    {
+        return current($this->all_items);
+    }
+
+    public function next()
+    {
+        next($this->all_items);
+    }
+
+    public function key()
+    {
+        return key($this->all_items);
+    }
+
+    public function valid()
+    {
+        return current($this->all_items);
+    }
+
+    public function rewind()
+    {
+        reset($this->all_items);
+    }
+}
--- src/Algorithm/FindCoupleByBirthdayDistance.php
+++ src/Algorithm/FindCoupleByBirthdayDistance.php
@@ -9,7 +9,7 @@ final class FindCoupleByBirthdayDistance
     /** @var Person[] */
     private $people;
 
-    /** @var Couple[] */
+    /** @var CoupleCollection */
     private $couples;
 
     /** @var BirthdayDistanceCriteria */
@@ -18,30 +18,18 @@ final class FindCoupleByBirthdayDistance
     public function __construct(array $people)
     {
         $this->people  = $people;
-        $this->couples = $this->getCouples($people);
+        $this->couples = CoupleCollection::buildAllPossibleCouplesFromPeopleArray($people);
     }
 
     public function findByBirthdaysDistance(int $find_criteria): Couple
     {
-        if (empty($this->couples)) {
+        if (0 == count($this->couples)) {
             return new CoupleEmpty();
         }
 
         $this->birthday_distance_criteria = BirthdayDistanceCriteria::fromCriteriaParameter($find_criteria);
 
         return $this->getBestMatchCouple();
-    }
-
-    private function getCouples(array $people): array
-    {
-        $couples = [];
-        for ($i = 0; $i < count($people); $i++) {
-            for ($j = $i + 1; $j < count($people); $j++) {
-                $couples[] = new Couple($people[$i], $people[$j]);
-            }
-        }
-
-        return $couples;
     }
 
     private function getBestMatchCouple(): Couple
